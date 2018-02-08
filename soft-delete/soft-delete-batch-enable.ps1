173f70acafc0f44b8eb62eeacd0899537c95c30b
MichaelHauss <mihauss@microsoft.com>
2018-02-07 16:42:27 -0800

    removed PS scripts

--- soft-delete/soft-delete-batch-enable.ps1
+++
@@ -1,3 +0,0 @@
-﻿Set-AzureRmContext -Subscription "<subscription-name>"
-$MatchingAccounts = Get-AzureRMStorageAccount | where-object{$_.StorageAccountName -match "<matching-regex>"}
-$MatchingAccounts | Enable-AzureStorageDeleteRetentionPolicy -RetentionDays 7
